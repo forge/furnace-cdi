3beac1675ef10f119d56561d57ce8441bba4e737
George Gastaldi <gegastaldi@gmail.com>
2015-05-05 16:55:50 -0300

    FURNACE-47: Added test for AddonRegistry.fireEvent

---
+++ tests/src/test/java/test/org/jboss/forge/furnace/addons/AddonRegistryEventTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package test.org.jboss.forge.furnace.addons;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.forge.furnace.addons.AddonRegistry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class AddonRegistryEventTest
+{
+   static boolean eventFired;
+
+   @Deployment
+   @AddonDependencies
+   public static AddonArchive getDeployment()
+   {
+      return ShrinkWrap.create(AddonArchive.class).addBeansXML();
+   }
+
+   @Inject
+   AddonRegistry addonRegistry;
+
+   @Test
+   public void testFireEvent()
+   {
+      addonRegistry.fireEvent("MYEVENT");
+      Assert.assertTrue("Event was not received", eventFired);
+   }
+
+   public void observeEvent(@Observes String event)
+   {
+      eventFired = "MYEVENT".equals(event);
+   }
+}
